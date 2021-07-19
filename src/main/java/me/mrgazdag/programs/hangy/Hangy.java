7b3287a5b6d421449e337904a7ce456258608dc2
MrGazdag <pumpkincraftszerver@gmail.com>
2021-07-19 14:46:20 +0200

    updating

--- src/main/java/me/mrgazdag/programs/hangy/Hangy.java
+++ src/main/java/me/mrgazdag/programs/hangy/Hangy.java
@@ -1,10 +1,33 @@
 package me.mrgazdag.programs.hangy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hangy {
+    private final HangyWorld world;
     private List<HangyTarget> targets;
-    public Hangy() {
+    private List<HangyTarget> route;
+    private HangyTarget lastNode;
+    private double distanceWalked;
+    public Hangy(HangyWorld world) {
+        this.world = world;
+    }
+
+    public void reset(List<HangyTarget> targets, HangyTarget lastNode) {
+        this.targets = new ArrayList<>(targets);
+        this.route = new ArrayList<>();
+        this.lastNode = lastNode;
+        this.targets.remove(lastNode);
+        this.distanceWalked = 0;
+    }
+
+    public double getDistanceWalked() {
+        return distanceWalked;
+    }
+
+    public void tick() {
+        double lx = lastNode.getXPos();
+        double ly = lastNode.getYPos();
 
     }
 }
--- src/main/java/me/mrgazdag/programs/hangy/HangyTarget.java
+++ src/main/java/me/mrgazdag/programs/hangy/HangyTarget.java
@@ -1,16 +1,47 @@
 package me.mrgazdag.programs.hangy;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class HangyTarget {
     private final double xPos;
     private final double yPos;
     private final String name;
     private double currentPheromone;
+    private Map<HangyTarget, Double> distanceMap;
+    private Set<Hangy> visited;
 
     public HangyTarget(double xPos, double yPos, String name) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.name = name;
         this.currentPheromone = 0;
+        this.distanceMap = new HashMap<>();
+        this.visited = new HashSet<>();
+    }
+    public double distanceTo(HangyTarget other) {
+        if (!distanceMap.containsKey(other)) {
+            double distance = Math.sqrt(Math.pow(other.xPos-xPos, 2) + Math.pow(other.yPos-yPos, 2));
+            this.distanceMap.put(other, distance);
+            other.distanceMap.put(this,distance);
+            return distance;
+        } else {
+            return distanceMap.get(other);
+        }
+    }
+
+    public boolean isVisited(Hangy hangy) {
+        return visited.contains(hangy);
+    }
+
+    public void visit(Hangy hangy) {
+        visited.add(hangy);
+    }
+
+    public void clearVisits() {
+        visited.clear();
     }
 
     public double getXPos() {
