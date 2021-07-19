cec08564338d99f323a61c3d88919a89241ba423
MrGazdag <pumpkincraftszerver@gmail.com>
2021-07-19 14:18:06 +0200

    hangyworld

--- src/main/java/me/mrgazdag/programs/hangy/HangyWorld.java
+++ src/main/java/me/mrgazdag/programs/hangy/HangyWorld.java
@@ -4,21 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HangyWorld {
-    private final List<HangyTarget> targets;
+    private final List<HangyTargetInstance> targets;
+    private final double xSize;
+    private final double ySize;
+    private final double pheromoneEvaporationRate;
+    private final int antsPerGroup;
+    private final List<Hangy> ants;
 
-    public HangyWorld() {
-        this.targets = new ArrayList<>();
-    }
-
-    public HangyWorld(List<HangyTarget> targets) {
+    public HangyWorld(List<HangyTargetInstance> targets, double xSize, double ySize, double pheromoneEvaporationRate, int antsPerGroup) {
         this.targets = targets;
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.pheromoneEvaporationRate = pheromoneEvaporationRate;
+        this.antsPerGroup = antsPerGroup;
+        this.ants = new ArrayList<>();
+        for (int i = 0; i < antsPerGroup; i++) {
+            ants.add(new Hangy());
+        }
     }
 
-    public void addPoint(HangyTarget target) {
+    public double getXSize() {
+        return xSize;
+    }
+
+    public double getYSize() {
+        return ySize;
+    }
+
+    public double getPheromoneEvaporationRate() {
+        return pheromoneEvaporationRate;
+    }
+
+    public void addPoint(HangyTargetInstance target) {
         targets.add(target);
     }
 
-    public List<HangyTarget> getTargets() {
+    public List<HangyTargetInstance> getTargets() {
         return targets;
+    }
+
+    public void tick() {
+
     }
 }
