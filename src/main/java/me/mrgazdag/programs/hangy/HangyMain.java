d0662a7ee5c4a62fbf5c161cb0fd6d0e0ac98f11
aprobeni <aprobeni.programs@gmail.com>
2021-07-19 14:50:53 +0200

    hagymain json fasz

--- src/main/java/me/mrgazdag/programs/hangy/HangyMain.java
+++ src/main/java/me/mrgazdag/programs/hangy/HangyMain.java
@@ -1,7 +1,52 @@
 package me.mrgazdag.programs.hangy;
 
-public class HangyMain {
-    public static void main(String[] args) {
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class HangyMain {
+    public static void main(String[] args) throws IOException {
+        HangyWorld world = loadFromJSON(Objects.requireNonNull(getJSONFromFile(new File(args[0]))));
+    }
+
+    @SuppressWarnings("MismatchedQueryAndUpdateOfStringBuilder")
+    public static JSONObject getJSONFromFile(File file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        StringBuilder sb = new StringBuilder();
+        while (br.ready()) {
+            sb.append(br.readLine()).append("\n");
+        }
+        br.close();
+        return new JSONObject(br.toString());
+    }
+
+    public static HangyWorld loadFromJSON(JSONObject json) {
+        JSONObject world = json.getJSONObject("world");
+        JSONArray targets = json.getJSONArray("targets");
+        List<HangyTarget> targetList = new ArrayList<>();
+        for (Object o : targets) {
+            if (o instanceof JSONObject obj) {
+                targetList.add(new HangyTarget(
+                        obj.getDouble("x"),
+                        obj.getDouble("y"),
+                        obj.optString("name", null)
+                ));
+            }
+        }
+        return new HangyWorld(
+                targetList,
+                world.getDouble("worldSizeX"),
+                world.getDouble("worldSizeY"),
+                world.getDouble("pheromoneEvaporationRate"),
+                world.getInt("antsPerGroup"),
+                world.getInt("startNodeIndex")
+        );
     }
 }
